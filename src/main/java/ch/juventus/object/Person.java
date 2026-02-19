5977bd05c9444e23a5dd531dea8daa378457c9d0
linda <lkrueger17@gmail.com>
2026-02-19 15:05:05 +0100

    Add java object exercise

---
+++ src/main/java/ch/juventus/object/Person.java
@@ -0,0 +1,79 @@
+package ch.juventus.object;
+
+import java.util.Objects;
+
+public class Person {
+
+    private final String firstName;
+    private final String lastName;
+    private final Address address;
+    private final int age;
+    private final boolean hasPet;
+
+    public Person(String firstName, String lastName, Address address, int age, boolean hasPet) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.age = age;
+        this.hasPet = hasPet;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = null;
+        this.age = 0;
+        this.hasPet = false;
+    }
+
+    public Person(String firstName, int age) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = "";
+        this.address = null;
+        this.hasPet = false;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isHasPet() {
+        return hasPet;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address=" + address +
+                ", age=" + age +
+                ", hasPet=" + hasPet +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && hasPet == person.hasPet && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, address, Integer.valueOf(age), Boolean.valueOf(hasPet));
+    }
+}
